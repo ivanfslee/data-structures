e05a3a3e0fa2b288fdd552c6449c85c6be836027
ivanfslee <ivanfslee@gmail.com>
2020-01-02 10:46:20 -0800

    Added insert method

--- Tree.js
+++ Tree.js
@@ -47,7 +47,41 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
+
+    insert(value) {
+        var newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+            return this; //return the tree
+        }
+        
+        var current = this.root;
+        while (true) {
+            if (value === current.value) { //this line handles duplicate values inserted into tree
+                return undefined;
+            }
+            if (value < current.value) {
+                if (current.left === null) {
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            } else if (value > current.value) {
+                if (current.right === null) {
+                    current.right = newNode;
+                    return this;
+                }
+                current = current.right;
+            }
+        }
+    }
 }
+
+//Note: handling duplicate values in tree
+//Ask interviewer how you should handle duplicates that are inserted into tree. Ignore them? return undefined? Add a count property?
+//One possibility is to add a count property to the nodes. If node encounters a duplicate, it will just increment the counter
+
+
 
 // Creating BST and manually adding nodes 
 // var tree = new BinarySearchTree();
@@ -55,3 +89,5 @@ class BinarySearchTree {
 // tree.root.right = new Node(15);
 // tree.root.left = new Node(7);
 // tree.root.left.right = new Node(9);
+
+//Insert method can be done iteratively or recursively 
