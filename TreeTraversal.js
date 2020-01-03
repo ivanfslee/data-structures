b4959bbe20a0969bb7d74dce444d7613ae59334b
ivanfslee <ivanfslee@gmail.com>
2020-01-03 14:58:29 -0800

    Added Tree Traversal  notes

---
+++ TreeTraversal.js
@@ -0,0 +1,15 @@
+//Given a tree (any tree) - BST, etc
+//How to visit every node one time?
+//Will make use of recursion heavily 
+
+//Two main ways to traverse a tree
+    //Breadth-first Search - going horizontally across each node level
+    //Depth-first Search
+        //DFS - PreOrder    
+        //DFS - InOrder
+        //DFS - PostOrder
+
+//DFS vs BFS - Do we prioritize visiting all nodes on same level before we move down ?
+//or do we want to work in a more vertical way with DFS?
+
+//Our approach on BFS vs DFS depends a lot on how tree is structured (e.g. 100+ children per node) and how it branches and how the data is distributed 
