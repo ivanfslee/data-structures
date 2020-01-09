7e7b1c793a7f80d65f4ce08d420a074f47db93c2
ivanfslee <ivanfslee@gmail.com>
2020-01-08 20:38:08 -0800

    Added set and get method

--- HashTable.js
+++ HashTable.js
@@ -28,7 +28,7 @@
 
 //How do collisions occur in a hash table
 
-//Handle collisions usuing separate chaining or linear probing 
+//Handle collisions using separate chaining or linear probing 
 
 //We will implement our own hash table 
     //keys in hashtables don't have to just be strings, but we will just use strings as keys in our implementation
@@ -81,3 +81,90 @@ function hash(key, arrayLen) { //arrayLen is length of array we are storing key-
     //1. only hashes strings (we won't address this)
     //2. not constant time - we loop through each character
     //3. could be a little more random 
+
+//Revised hash function 
+function hash(key, arrayLen) { //hash functions almost alway use prime numbers because it reduces collisions. arrayLen variable passed in should be a prime number  
+    let total = 0;
+    let WEIRD_PRIME = 31; //we use prime number 
+    for (let i =  0; i < Math.min(key.length, 100); i ++) { //the conditional takes the min of key length or 100. That is, if key is larger than 100 characters, the most loops for loop would do is 100
+        let char = key[i];
+        let value = char.charCodeAt(0) - 96;
+        total = (total * WEIRD_PRIME + value) % arrayLen;
+    }
+    return total;
+}
+
+// Why Prime Numbers?
+    //if length of your hash table (the number of key-value pairs) is prime - the less likely for collisions
+    //Prime number in the hash is helpful in spreading out the keys more uniformly
+    //How do prime numbers contribute to this? Math is complicated
+
+//What are collisions?
+    //When our hash algorithm returns the same thing for two different inputs
+    // e.g.  hash("cyan", 13)  -> returns 5
+    // e.g.  hash("pink", 13)  -> returns 5 
+    // This would be a collision.
+
+// Dealing with Collisions
+    //1. Separate chaining - at each index in our array (hash table), we store values using more sophisticated data structure (e.g. array or linked list). 
+        //This allows us to store multiple key value pairs at one particular index 
+        //Thus, we are able to store more than the length of our array
+    //2. Linear Probing - we only store one piece of data at each position in the array. When we encounter a collision, we look for the next empty slot and put the value there.
+
+
+//Hash Table Class
+
+class HashTable {
+    constructor(size = 53) { //default array size of 53, a prime number 
+        this.keyMap = new Array(size);
+    }
+
+    _hash(key) {
+        let total = 0;
+        let WEIRD_PRIME = 31;
+        for (let i = 0; i < Math.min(key.length, 100); i++) {
+            let char = key[i];
+            let value = char.charCodeAt(0) - 96;
+            total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+        }
+        return total;
+    }
+
+    set(key, value) {
+        let index = this._hash(key);
+        if (!this.keyMap[index]) { //if nothing at the index, put an empty array at that index
+            this.keyMap[index] = [];
+        }
+        this.keyMap[index].push([key, value]); //push an array with key and value at that index 
+    }
+
+    get(key) {
+        let index = this._hash(key); //hash the key
+        if (this.keyMap[index]) { //if there is something at this index
+            for (let i = 0; i < this.keyMap[index].length; i++) { //loop through each element at the index and look for the matching key
+                let currentElem = this.keyMap[index][i]
+                if (currentElem[0] === key) { //if currentElem's key matches the key
+                    return currentElem[1]; //return value 
+                }
+            }
+        }
+        return undefined; //if nothing at the index, return undefined 
+    }
+
+}
+
+//Set and Get Methods
+    //Set 
+        //1. Accept a key and a value
+        //2. Hashes the key -> get an index to see where in array/hash table it will be stored
+        //3. Store the key-value pair in hash table array via separate chaining 
+    
+    //Get
+        //1. Accepts a key 
+        //2. Hashes the key -> we get index back
+        //3. Look through that index and find the key that matches - return the key-value pair
+        //4. If key not found, return undefined
+
+//Keys and Values Methods
+    //Keys
+        //1. 
