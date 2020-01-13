bc200a4f8f287b8d8622edf6f3fd44e3db95b6f3
ivanfslee <ivanfslee@gmail.com>
2020-01-13 13:43:43 -0800

    Added priorityQueue class to dequeue lowest priority vertex

--- DijkstrasAlgorithm.js
+++ DijkstrasAlgorithm.js
@@ -34,13 +34,46 @@ class WeightedGraph {
     // {
     //     A: [{node: 'B', weight: 10}]
     // }
-
-
 }
+
+
+//this implementation is not as good as binary heap implementation (which we will do later)
+//we need a priority queue in order to find the next lowest vertex to our origin 
+//when we dequeue (after sorting the vertices by priority), this will give us the lowest vertex to origin 
+class PriorityQueue {
+    constructor() {
+        this.values = [];
+    }
+
+    enqueue(val, priority) {
+        this.values.push({val, priority});
+        this.sort();
+    }
+
+    dequeue() {
+        return this.values.shift();
+    }
+
+    sort() { //sort method used internally
+        this.values.sort((a, b) => a.priority - b.priority); //we are sorting here so O(N * log(N))
+    }
+}
+
 
 
 var graph = new WeightedGraph();
 graph.addVertex('A');
 graph.addVertex('B');
 graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
+graph.addVertex('F');
 
+graph.addEdge('A', 'B', 4);
+graph.addEdge('A', 'C', 2);
+graph.addEdge('B', 'E', 3);
+graph.addEdge('C', 'D', 2);
+graph.addEdge('C', 'F', 4);
+graph.addEdge('D', 'E', 3);
+graph.addEdge('D', 'F', 1);
+graph.addEdge('E', 'F', 1);
