5e43639e2e5b61847d76340ffb7a27a634b7bbea
ivanfslee <ivanfslee@gmail.com>
2020-01-15 15:42:49 -0800

    Added algo problem solving pattern notes

---
+++ Algo&PSPatterns.js
@@ -0,0 +1,24 @@
+//Algorithms and Problem Solving Patterns
+
+//What is an algorithm?
+    //a process or set of steps to accomplish a certain task
+    //everything in programming involves some kind of algorithm
+    //it is the foundation for being a successful problem solver and developer
+
+//Devise a plan to solve algorithms
+    //How do you improve?
+        //1. Devise a plan for solving problems
+        //2. Master common problem solving patterns 
+
+    //IOCE - input/output/constraints/edge cases
+    // Step 1 - Understand the problem
+    // Step 2 - Explore concrete examples
+    // Step 3 - Break it down
+    // Step 4 - Solve/simplify
+    // Step 5 - Look back and refactor 
+
+
+//Compare and contrast problem solving patterns 
+    //frequency counters
+    //two pointer problems
+    //divide and conquer
