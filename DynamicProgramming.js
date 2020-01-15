f4b5fb8b47beb5b0f8303c9934d14158c7ac7de5
ivanfslee <ivanfslee@gmail.com>
2020-01-14 21:17:28 -0800

    Added memo-ized fibonacci function

--- DynamicProgramming.js
+++ DynamicProgramming.js
@@ -32,3 +32,31 @@ function fib(n) {
 //Big O of fibonacci recursion solution 
     // approximately O(2^n) exponential!!! time complexity
     // closer value is 1.6^n time complexity
+
+//Memoization!!!!
+    //Remembering previously calculated values
+    //Memoization - storing the results of expensive function calls and returning the cached result when the same inputs occur again
+
+
+//Memo-ized solution for fibonacci sequence
+    //Big O:
+        //Recursive Fibo - O(2^N) - exponential time complexity
+        //Memo-ized Fibo - looking up value in array is constant time 
+            //O(N) time complexity for memo-ized fibonacci 
+function fib(n, memo = []) { //memo stores fib(n) at the nth index in memo array
+    if (memo[n] !== undefined) { //if we have fib(n) in memo at index n, return that value
+        return memo[n];
+    }
+
+    if (n <= 2) { //base case 
+        return 1;
+    }
+
+    var res = fib(n - 1, memo) + fib(n - 2, memo); //fib calls store in variable 'res' and stored in memo
+    memo[n] = res;
+    return res;
+}
+
+//alternative is using an obj as memo 
+//alternative is preload base base case
+    //function fib(n, memo = [undefined, 1, 1]) - with this you don't need lines 47-49
