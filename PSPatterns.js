021d26b0d40aaa564051f779c4e91993a58530b6
ivanfslee <ivanfslee@gmail.com>
2020-01-18 13:12:07 -0800

    Added other patterns

--- PSPatterns.js
+++ PSPatterns.js
@@ -19,7 +19,21 @@
     //someArray.indexOf(someArray[i]);  //indexOf method is O(N) time 
 
 //Multiple Pointers Pattern
+    //creating pointers or values that correspond to an index or position
+    //and move towards the beginning, end, or middle based on a certain condition
+    //VERY efficient for solving problems - O(N) typically  
+    //Minimal space complexity as well - O(1) typically 
+
 
 // Sliding Window Pattern
+    //This pattern involves creating a window
+    //which can either be an array or number from one position to another
+    //Depending on a certain condition, the window either increases or closes 
+    //(and a new window is created)
+    //Very useful for keeping track of a subset of data that is continious in an array/string etc
+
 
 //Divide and Conquer 
+    //This pattern involves dividing a data set into smaller chunks
+    //Then repeating a process with a subset of data
+    //This pattern can tremendously DECREASE time complexity
