0158c550174cad4a32873c78bf496086eb98b4b8
ivanfslee <ivanfslee@gmail.com>
2020-01-18 01:05:50 -0800

    Added notes about problem solving patterns

--- PSPatterns.js
+++ PSPatterns.js
@@ -11,8 +11,15 @@
         //Declarative Programming??
         //Functional Programming?
 
-//Frequency Counters
+//Frequency Counters Pattery
     //This pattern uses objects or sets to collect values/frequencies of values
     //This can often avoid the need for nested loops or O(N^2) operations with arrays/strings
     //Useful if you have multiple inputs and need to compare/contrast them
- 
+    //Typically is O(N) time (naive solution of nested for loops is O(N^2) time)
+    //someArray.indexOf(someArray[i]);  //indexOf method is O(N) time 
+
+//Multiple Pointers Pattern
+
+// Sliding Window Pattern
+
+//Divide and Conquer 
