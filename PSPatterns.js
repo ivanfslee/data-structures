d1b98662a18e51e7d4047b04831b45a88657c6d8
ivanfslee <ivanfslee@gmail.com>
2020-01-17 20:07:36 -0800

    Added pspattern notes

---
+++ PSPatterns.js
@@ -0,0 +1,18 @@
+//Some patterns 
+    //Frequency counter
+    //Multiple pointers
+    //Sliding window
+    //Divide and Conquer
+    //Dynamic Programming
+        //Memoization
+    //Greedy Algorithms
+    //Backtracking 
+    //Others~
+        //Declarative Programming??
+        //Functional Programming?
+
+//Frequency Counters
+    //This pattern uses objects or sets to collect values/frequencies of values
+    //This can often avoid the need for nested loops or O(N^2) operations with arrays/strings
+    //Useful if you have multiple inputs and need to compare/contrast them
+ 
