95f8d1a9a880b5c46e8839561f6bf995122d7552
ivanfslee <ivanfslee@gmail.com>
2020-01-22 00:04:08 -0800

    Added general sorting algorithm notes

---
+++ BubbleSort.js
@@ -0,0 +1,30 @@
+//Introduction to Sorting Algorithms 
+
+//Elementary Sorting Algorithms 
+
+//What is sorting?
+    //Sorting is the process of rearranging the items in a collection (e.g. an array) 
+    //so that the items are in some kind of order
+
+    //e.g. sorting numbers from smallest to largest
+    //sort names alphabetically
+    //sort movies based based on release year 
+    //sort movies based on revenue 
+
+//Why learn sorting?
+    //sorting is an incredibly common task - good to know how it works
+    //there are many different ways to sort things
+    //and different techniqes have their own advantages and disadvantages 
+
+    //it also depends on our data
+        //on totally random data insertion sort is slow
+        //but on nearly sorted data, insertion sort is fast
+        //https://www.toptal.com/developers/sorting-algorithms
+
+//JavaScript's Sort Method 
+//Elementary Sorting Algorithms 
+    //less commonly used because they are less performant or are only good in certain situations 
+    //bubble sort
+    //selection sort
+    //insertion sort 
+
