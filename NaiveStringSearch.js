0866a85688421ae9d25083c8e72598ac924f4c6d
ivanfslee <ivanfslee@gmail.com>
2020-01-21 18:49:15 -0800

    Added naiveStringSearch implementation

--- NaiveStringSearch.js
+++ NaiveStringSearch.js
@@ -4,11 +4,11 @@
 function naiveSearch(long, short) {
     let count = 0;
     for (let i = 0; i < long.length; i++) {
-        for (let j = 0; j < short.length; j++) {
-            if (short[j] !== long[i + j]) {
-                break;
+        for (let j = 0; j < short.length; j++) { //iterates over the short string 
+            if (short[j] !== long[i + j]) { //compares the short string to the long string - breaks out of inner j loop only if characters are not equal 
+                break; //this break only breaks out of the inner j for loop and will continue with i for loop
             }
-            if (j === short.length - 1) {
+            if (j === short.length - 1) { //if j reaches end of length of short, it will increment count variable 
                 count++;
             }
         }
