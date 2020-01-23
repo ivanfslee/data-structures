54631819e342c98dbc6a105a5f1d08852f42d83b
ivanfslee <ivanfslee@gmail.com>
2020-01-22 17:50:39 -0800

    Added JavaScript sort method notes

--- BubbleSort.js
+++ BubbleSort.js
@@ -22,6 +22,22 @@
         //https://www.toptal.com/developers/sorting-algorithms
 
 //JavaScript's Sort Method 
+    //with letters
+    ['Steele', 'Colt', 'Data Structures', 'Algorithms'].sort();
+    //Returns
+    ['Algorithms', 'Colt', 'Data Structures', 'Steele']
+
+    //with numbers
+    [ 6, 4, 15, 10].sort();
+    //Returns
+    [10, 15, 4, 6]
+    //The default sort order is according to string unicode points 
+    //unicode is used to represent strings
+    //every element in the array is converted to a string
+    //then unicode value of that is taken and then they're sorted
+
+
+
 //Elementary Sorting Algorithms 
     //less commonly used because they are less performant or are only good in certain situations 
     //bubble sort
