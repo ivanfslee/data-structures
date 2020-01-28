1cc8ee03ca5cc2c07e27dfc6ecbccaf9682a0b4e
ivanfslee <ivanfslee@gmail.com>
2020-01-28 10:29:59 -0800

    Added Big O of radix sort

--- RadixSort.js
+++ RadixSort.js
@@ -16,7 +16,26 @@
 
 //Radix Sort 
     //Special sorting algorithm that works on lists of numbers 
-    //It never makes comparisons between elements 
+    //It never makes direct comparisons between elements 
         //e.g. x is greater/less than y 
     //It exploits the fact that information about the size of a number
-    //is encoded in the number of digits 
+    //is encoded in the number of digits the number has 
+
+//Big O - Radix Sort 
+    //n is length of array we are sorting
+    //k is length of those numbers (i.e. number of digits)
+    //k matters because number of digits may be very large 
+
+    //caveat - theoretically, radix sort can be faster than any of the 
+    //comparison sorts, but because of limitations on how computers actually store
+    //numbers in memory, this may not actually be the case
+    // k's limit is log N 
+    
+    //Time Complexity (best)
+        //O(nk)
+    //Time Complexity (average)
+        //O(nk)
+    //Time Complexity (worst)
+        //O(nk)
+    //Space Complexity 
+        //O(n + k)
