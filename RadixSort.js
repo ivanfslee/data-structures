7fb8c1713f7e5f4eb382c71780bb16f9c032ad6b
ivanfslee <ivanfslee@gmail.com>
2020-01-29 13:10:31 -0800

    Added radixSort helper functions

--- RadixSort.js
+++ RadixSort.js
@@ -47,4 +47,47 @@
     //getDigit(12345, 2); //3
 
     //Convert number to a string, then use correct index to access number you want and then convert back to a number
-     
+
+function getDigit(num, i) {
+    //divides the num by 10, 100, 1000 etc. Whatever the 10's place the digit is at
+    //Math.abs is so that it works with negative numbers as well
+    return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+}   
+
+getDigit(7323, 2);
+// 7323/100 = 73.23
+//Math.floor(73.23) = 73
+// 73 % 10 = 3
+//getDigit(7323, 2) returns 3 which is the 2 digit 
+
+
+//Another helper function we use - digitCount - figures out how many digits in a number 
+    //digitCount(num) - returns the number of digits in num 
+    //digitCount(1) // 1
+    //digitCount(25) // 2
+    //digitCount(314) // 3 
+
+function digitCount(num) { //num is a base 10 number
+    if (num === 0) return 1; //special case for when num is 0 
+    return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+//note: log10(x) means 10 to what power gives us x 
+//Math.log10(423) = 2.62634 
+//Math.floor(2.62634) = 2
+// 2 + 1 = 3
+
+//Another helper function we use - mostDigits(nums) - given an array of numbers, returns the number of digits of the largest number in the list 
+    //mostDigits([1234, 56, 7]); // 4
+    //mostDigits([1, 1, 11111, 1]); // 5
+    //mostDigits([12, 34, 56, 78]); // 2
+
+function mostDigits(nums) {
+    var highestDigits = 0;
+    for (var i = 0; i < nums.length; i++) {
+        if (digitCount(nums[i]) > highestDigits) {
+            highestDigits = digitCount(nums[i]);
+        }
+    }
+    return highestDigits;
+}
