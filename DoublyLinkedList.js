9e0afed44a7756c9171c31dfb85d4f4d19c7986d
ivanfslee <ivanfslee@gmail.com>
2020-01-30 11:17:30 -0800

    Added Doubly Linked Lists'

---
+++ DoublyLinkedList.js
@@ -0,0 +1,24 @@
+//Construct a doubly linked list
+//Compare and contrast Doubly and Singly Linked Lists
+//Implement basic operations on a doubly linked list 
+    //push
+    //pop
+    //shift
+    //unshift
+    //insert
+    //get
+    //set
+
+
+//Doubly Linked List 
+    //Almost identical to Singly Linked List
+    //Except every node has another pointer
+    //to the previous node
+
+    //head points to null and next node
+    //tail points to previous node and null 
+
+    //Removing last node is alot easier with Doubly Linked Lists
+    //You can go to the tail and then go to the previous node to remove the tail
+    //With Singly Linked List, to remove last node, you have to go through the whole list
+    //and get to the second to the last element, in order to remove the tail. 
