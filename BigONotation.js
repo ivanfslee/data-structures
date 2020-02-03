f9c8c56d4e412b6fe034f2b563f6473eb64c64f9
ivanfslee <ivanfslee@gmail.com>
2020-02-03 11:20:19 -0800

    Added Big O notation notes

---
+++ BigONotation.js
@@ -0,0 +1,13 @@
+//What is the need for Big O notation?
+    //A way to compare different implementations of code 
+    //You can use it discuss tradeoffs between different approaches (serves as an analytical tool)
+    //When your code slows down or crashes, you can pinpoint inefficiences and bottlenecks in your applications
+//What is Big O Notation?
+
+//Simplify Big O Expressions
+
+//Define 'time complexity' and 'space complexity'
+
+//Evaluate time and space complexity of different algorithms using Big O notation
+
+//What a logarithm is
