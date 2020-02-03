099b299592d1a4d80c488107532d3ac7f31a39d5
ivanfslee <ivanfslee@gmail.com>
2020-02-03 15:21:59 -0800

    Added Algo Expert notes on Complexity Analysis

--- BigONotation.js
+++ BigONotation.js
@@ -2,7 +2,15 @@
     //A way to compare different implementations of code 
     //You can use it discuss tradeoffs between different approaches (serves as an analytical tool)
     //When your code slows down or crashes, you can pinpoint inefficiences and bottlenecks in your applications
+
+    //We don't use time that algo takes-
+        //Different machines will record different times
+        //Same machine will record different times
+        //For particularly fast algos, speed measurements may not be precise enough
+
 //What is Big O Notation?
+    //Count the number of simple operations the computer performs 
+    //Allows us to talk formally about how runtime of an algo grows as input grows
 
 //Simplify Big O Expressions
 
@@ -11,3 +19,30 @@
 //Evaluate time and space complexity of different algorithms using Big O notation
 
 //What a logarithm is
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+//Algo Expert - Complexity Analysis
+    //Time Complexity & Space Complexity
+        //Time Complexity 
+            //Measure of how fast an algo runs
+        //Space Complexity
+            //Measure of how much memory/space an algo takes up 
+
+    // One Problem - Multiple Solutions - How do you compare them? 
+        //What makes one solution better than another solution?
+            //Whether or not solution has a better time and/or space complexity
+            //S1 - solution 1
+            //S2 - solution 2
+            //S3 - solution 3
+
+        //What if one solution has better time, but another solution has better space?
+            //Then that would depend on the use case of the algorithm
+            //Which is more important in how the alforithm is used? 
+            //Is time more important? 
+            //Or is space more important?
+
+    //What data structure best allows you to solve problem with best time and space complexity
+    
+//Algo Expert - Big O Notation
