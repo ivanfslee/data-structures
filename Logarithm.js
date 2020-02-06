9aabb854f61969a56134d1cd5e6cea24560ff815
ivanfslee <ivanfslee@gmail.com>
2020-02-05 22:37:53 -0800

    Added Logarithm Algo Expert

---
+++ Logarithm.js
@@ -0,0 +1,9 @@
+//Logarithm
+    //logarithm of x of base b equals y if and only if b to the power of y equals x 
+    // log (x) = y    iif  b^y = x
+    //    b
+
+
+
+//In computer science, the base is typically base 2 
+//log (N) = y iif 2^y = N 
