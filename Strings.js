f58afc097d87fb730f0d3d4860b74c8e2ad93036
ivanfslee <ivanfslee@gmail.com>
2020-02-11 15:16:30 -0800

    Added strings notes from algo expert

---
+++ Strings.js
@@ -0,0 +1,12 @@
+//Algo Expert Notes on Strings
+    //strings are typically known as a data type
+    //strings also can behave like a data structure
+        //that we manipulate that has its own time and space complexity
+
+    //Strings are implemented differently depending on the programming language being used
+        //recall - string is stored in memory as an array of integers
+        //each integer corresponds to a character mapped by a character encoding standard - e.g. ascii
+        //'A' maps to 65
+        //'B' maps to 66
+        //'a' maps to 97
+        //ASCII standard has fewer than 256 
