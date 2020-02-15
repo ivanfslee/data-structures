3a67a5e85430393c160aa51ab65ca6fb8bb1a92a
ivanfslee <ivanfslee@gmail.com>
2020-02-15 15:01:05 -0800

    Added Roughgarden notes 1-1

---
+++ RoughgardenAlgorithms/1-1-WhyStudyAlgo.js
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////////////////////
+//Tim Roughgarden Notes
+    //Video 1 - 1
+    //Why study algorithms?
+    //Uses of Algorithms
+        //Routing and communication networks piggyback on classical shortest path algorithms
+        //Public key cryptography
+        //Database indices rely on balanced search tree data structures
+        //Computational biology uses dynamic programming algorithms to measure genome similarity
+    //Algorithms - play a key role in modern technological innovation
+        //Performance gains due to algorithms exceeded the dramatic performance gains due to increased processor speeds 
+    
+    //Video 1 - 2
+    //Integer Multiplication 
+    //Transform an input - 2 numbers, into an output, their product 
+
+    //IOCE - input/output/constraints/edge cases
+
+    //Pattern for the course
+        //1. Define a computational problem
+        //2. Say what input is, say what output is 
+        //3. Give a solution, giving an algorithm that transforms the input to the output
+    
